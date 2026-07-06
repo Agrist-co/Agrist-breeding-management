3315ac65791d6049ce8ef021f5b731110c1ef83d
Agrist-co <agrist.co@gmail.com>
2026-07-06 18:26:33 +0900

    Update daily_sheet.py

--- daily_sheet.py
+++ daily_sheet.py
@@ -639,7 +639,8 @@ with tab1:
             brand_nm = _brand_full.split("_")[-1] if "_" in _brand_full else _brand_full
         else:
             # feed_brand_idがない場合は日齢から自動判定
-            _auto_brand = get_brand_for_age(age, active_brs)
+            _active_brs_disp = [b for b in feed_brands if b.get("is_active") not in (None, False, 0, "false", "0", "")]
+            _auto_brand = get_brand_for_age(age, _active_brs_disp)
             _brand_full = _auto_brand["brand_name"] if _auto_brand else ""
             brand_nm = _brand_full.split("_")[-1] if "_" in _brand_full else _brand_full
         brand_obj = next((b for b in feed_brands if b["feed_brand_id"] == brand_id), {}) if brand_id else {}
